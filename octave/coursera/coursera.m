a22ef70a16a6d801d0ffca60f4cb540b539553fd
vladmld <vlad.moldoveanu@protonmail.com>
2021-04-25 21:46:09 +0300

    add octave projects

---
+++ octave/coursera/coursera.m
@@ -0,0 +1,11 @@
+% Initialize matrix A and B 
+A = [1, 2, 4; 5, 3, 2]
+B = [1, 3, 4; 1, 1, 1]
+
+% Initialize constant s 
+s = 2
+
+% See how element-wise addition works
+add_AB = A + B 
+
+…add_As = A + s
